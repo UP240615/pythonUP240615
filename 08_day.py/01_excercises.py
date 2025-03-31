c7a29630ee2f273044f94b1ffc7de1780eff5551
UP240615 <up240615@alumnos.upa.edu.mx>
2025-03-31 13:19:06 -0600

    8

---
+++ 08_day.py/01_excercises.py
@@ -0,0 +1,53 @@
+perro = {}
+
+perro = {'Nombre':'Luna', 'Color':'Canelita', 'Raza':'husky', 'patas':'4', 'edad':'2'}
+
+estudiante= {'first_name':'Jhoana', 'last_name':'estephany', 'sexo':'mujer', 'edad':'19', 
+    'estado civil':'soltera', 'habilidades':['leer','dibujar', 'aprendo rapido','escribir'], 'pais':'mexico', 'ciudad':'aguascalientes', 
+      'direccion':'la casita'}
+
+estudiante= {'first_name':'Jhoana', 'last_name':'estephany', 'sexo':'mujer', 'edad':'19', 
+    'estado civil':'soltera', 'habilidades':'leer', 'pais':'mexico', 'ciudad':'aguascalientes', 
+      'direccion':'la casita'}
+print(len(estudiante))
+
+estudiante= {'first_name':'Jhoana', 'last_name':'estephany', 'sexo':'mujer', 'edad':'19', 
+    'estado civil':'soltera', 'habilidades':['leer','dibujar', 'aprendo rapido','escribir'], 'pais':'mexico', 'ciudad':'aguascalientes', 
+      'direccion':'la casita'}
+values = estudiante.values()
+print(['leer','dibujar', 'aprendo rapido','escribir'])
+print(type(estudiante))
+print(type(['leer','dibujar', 'aprendo rapido','escribir']))
+print(type("habilidades"))
+
+estudiante= {'first_name':'Jhoana', 'last_name':'estephany', 'sexo':'mujer', 'edad':'19', 
+    'estado civil':'soltera', 'habilidades':['leer','dibujar', 'aprendo rapido','escribir'], 'pais':'mexico', 'ciudad':'aguascalientes', 
+      'direccion':'la casita'}
+
+estudiante ['habilidades'].append('banda de Guerra')
+print(estudiante['habilidades'])
+
+estudiante= {'first_name':'Jhoana', 'last_name':'estephany', 'sexo':'mujer', 'edad':'19', 
+    'estado civil':'soltera', 'habilidades':['leer','dibujar', 'aprendo rapido','escribir'], 'pais':'mexico', 'ciudad':'aguascalientes', 
+      'direccion':'la casita'}
+keys = estudiante.keys()
+print(keys)
+
+values = estudiante.values()
+print(values)
+
+estudiante= {'first_name':'Jhoana', 'last_name':'estephany', 'sexo':'mujer', 'edad':'19', 
+    'estado civil':'soltera', 'habilidades':['leer','dibujar', 'aprendo rapido','escribir'], 'pais':'mexico', 'ciudad':'aguascalientes', 
+      'direccion':'la casita'}
+print(estudiante.items())
+
+estudiante= {'first_name':'Jhoana', 'last_name':'estephany', 'sexo':'mujer', 'edad':'19', 
+    'estado civil':'soltera', 'habilidades':['leer','dibujar', 'aprendo rapido','escribir'], 'pais':'mexico', 'ciudad':'aguascalientes', 
+      'direccion':'la casita'}
+estudiante.pop('direccion')
+
+del perro
+
+
+
+
